8441b68891ccff9065c23465c94e60451dce3ad2
Fisherman August <fshrmn.august@gmail.com>
2025-08-10 10:53:12 +0300

    models

--- server.go
+++ server.go
@@ -1,5 +1,27 @@
 package main
 
+import "net/http"
+
+type Agent struct {
+	Id int
+	Name string
+	LastTimeOnline string
+}
+
+type Task struct {
+	Id int
+	AgentName string
+	TaskCreationTime string
+	Text string
+	Result string
+	ResultTime string
+}
+
+
+
+
 func main(){
-	
+	http.HandleFunc("/", indexHandler) //просмотр таблицы агентов, создание нового агента
+	http.HandleFunc("/tasks", tasksHandler) //просмотр таблицы задач, обновление задачи
+	http.HandleFunc("/create_task", createTaskHandler) //просмотр формы создания задачи, создание новой задачи
 }
